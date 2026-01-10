293a5b1c29fe27e190e3fbf0a725c6fbcac1cb6c
Nabhan <nabs11004@gmail.com>
2026-01-10 20:59:30 +0400

    sorting logic added

--- logic/filters.js
+++ logic/filters.js
@@ -1,11 +1,12 @@
 export function filterBySearchTerm(fragrances, searchTerm) {
-    return fragrances.flter(f => 
+    return fragrances.filter(f => 
         f.name.toLowerCase().includes(searchTerm.toLowerCase())
     )
 }
 
 export function filterByBrand(fragrances, brand) {
+    if (brand === "") return fragrances;
     return fragrances.filter(f => 
-        f.brand.toLowerCase().includes(brand.toLowerCase())
-    )
+        f.brand.toLowerCase() === brand.toLowerCase()
+    );
 }
